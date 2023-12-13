78b278fe8ec314566c86ee05c9186feff58454f8
ohddang <ohddang509@gmail.com>
2023-12-13 14:36:30 +0900

    url path 수정

--- src/App.js
+++ src/App.js
@@ -7,18 +7,14 @@ import Home from './pages/home';
 import Ingame from './pages/ingame'
 import Ranking from './pages/ranking'
 
-const root_path = "this-is-blog/react-tetris/tetris";
-const ingame_path = `${root_path}/ingame`;
-const ranking_path = `${root_path}/ranking`;
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes basename={root_path}>
-        <Route path={root_path} element={<Home />}  />
-        <Route path={ingame_path} element={<Ingame />} />
-        <Route path={ranking_path} element={<Ranking />} />
+      <Routes>
+        <Route path='/' element={<Home />}  />
+        <Route path='/ingame' element={<Ingame />} />
+        <Route path='/ranking' element={<Ranking />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const root_path = "this-is-blog/react-tetris/tetris";
-const ingame_path = `${root_path}/ingame`;
-const ranking_path = `${root_path}/ranking`;
-
 function Home() {
   return (
     <>
       <h3>Home</h3>
       <ul>
-        <li><Link to={ingame_path}>인게임</Link></li>
-        <li><Link to={ranking_path}>랭킹</Link></li>
+        <li><Link to='/ingame'>인게임</Link></li>
+        <li><Link to='/ranking'>랭킹</Link></li>
       </ul>
     </>
   );
