908d737a503c26c2af8d9ead53d3e75eecf7d2da
ohddang <ohddang509@gmail.com>
2023-12-19 22:01:37 +0900

    styled component 적용 대문자 문제

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Ingame from './pages/ingame'
+import styled from 'styled-components'
+
+const StyledIngame = styled(Ingame)`
+  width:100%;
+  height: 100%;
+
+  background-color: #9caa84;
+  text-align: center;  
+`;
 
 function App() {
   return (
-    <Ingame />
+    <StyledIngame />
   );
 }
 
